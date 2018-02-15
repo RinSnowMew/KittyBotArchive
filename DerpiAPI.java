dbb247bdf1e99e8839eac6f448c5d01dd3cad104
Unknown <illkillyouwithmybrain@gmail.com>
2018-02-15 02:08:09 -0500

    Initial Derpi Search
    Beta Derpibooru searching

---
+++ DerpiAPI.java
@@ -0,0 +1,328 @@
+package main.java.net.dv8tion;
+
+public class DerpiAPI
+{
+    private String is_optimized;
+
+    private String source_url;
+
+    private String sha512_hash;
+
+    private String score;
+
+    private String file_name;
+
+    private String upvotes;
+
+    private String id;
+
+    private String[] tag_ids;
+
+    private String height;
+
+    private String[] duplicate_reports;
+
+    private String mime_type;
+
+    private String description;
+
+    private String created_at;
+
+    private String original_format;
+
+    private String aspect_ratio;
+
+    private String comment_count;
+
+    private String tags;
+
+    private String first_seen_at;
+
+    private String width;
+
+    private String uploader;
+
+    private String image;
+
+    private String downvotes;
+
+    private String updated_at;
+
+    private String is_rendered;
+
+    private String orig_sha512_hash;
+
+    private String faves;
+
+    private String uploader_id;
+
+    public String getIs_optimized ()
+    {
+        return is_optimized;
+    }
+
+    public void setIs_optimized (String is_optimized)
+    {
+        this.is_optimized = is_optimized;
+    }
+
+    public String getSource_url ()
+    {
+        return source_url;
+    }
+
+    public void setSource_url (String source_url)
+    {
+        this.source_url = source_url;
+    }
+
+    public String getSha512_hash ()
+    {
+        return sha512_hash;
+    }
+
+    public void setSha512_hash (String sha512_hash)
+    {
+        this.sha512_hash = sha512_hash;
+    }
+
+    public String getScore ()
+    {
+        return score;
+    }
+
+    public void setScore (String score)
+    {
+        this.score = score;
+    }
+
+    public String getFile_name ()
+    {
+        return file_name;
+    }
+
+    public void setFile_name (String file_name)
+    {
+        this.file_name = file_name;
+    }
+
+    public String getUpvotes ()
+    {
+        return upvotes;
+    }
+
+    public void setUpvotes (String upvotes)
+    {
+        this.upvotes = upvotes;
+    }
+
+    public String getId ()
+    {
+        return id;
+    }
+
+    public void setId (String id)
+    {
+        this.id = id;
+    }
+
+    public String[] getTag_ids ()
+    {
+        return tag_ids;
+    }
+
+    public void setTag_ids (String[] tag_ids)
+    {
+        this.tag_ids = tag_ids;
+    }
+
+    public String getHeight ()
+    {
+        return height;
+    }
+
+    public void setHeight (String height)
+    {
+        this.height = height;
+    }
+
+    public String[] getDuplicate_reports ()
+    {
+        return duplicate_reports;
+    }
+
+    public void setDuplicate_reports (String[] duplicate_reports)
+    {
+        this.duplicate_reports = duplicate_reports;
+    }
+
+    public String getMime_type ()
+    {
+        return mime_type;
+    }
+
+    public void setMime_type (String mime_type)
+    {
+        this.mime_type = mime_type;
+    }
+
+    public String getDescription ()
+    {
+        return description;
+    }
+
+    public void setDescription (String description)
+    {
+        this.description = description;
+    }
+
+    public String getCreated_at ()
+    {
+        return created_at;
+    }
+
+    public void setCreated_at (String created_at)
+    {
+        this.created_at = created_at;
+    }
+    
+    public String getOriginal_format ()
+    {
+        return original_format;
+    }
+
+    public void setOriginal_format (String original_format)
+    {
+        this.original_format = original_format;
+    }
+
+    public String getAspect_ratio ()
+    {
+        return aspect_ratio;
+    }
+
+    public void setAspect_ratio (String aspect_ratio)
+    {
+        this.aspect_ratio = aspect_ratio;
+    }
+
+    public String getComment_count ()
+    {
+        return comment_count;
+    }
+
+    public void setComment_count (String comment_count)
+    {
+        this.comment_count = comment_count;
+    }
+
+    public String getTags ()
+    {
+        return tags;
+    }
+
+    public void setTags (String tags)
+    {
+        this.tags = tags;
+    }
+
+    public String getFirst_seen_at ()
+    {
+        return first_seen_at;
+    }
+
+    public void setFirst_seen_at (String first_seen_at)
+    {
+        this.first_seen_at = first_seen_at;
+    }
+
+    public String getWidth ()
+    {
+        return width;
+    }
+
+    public void setWidth (String width)
+    {
+        this.width = width;
+    }
+
+    public String getUploader ()
+    {
+        return uploader;
+    }
+
+    public void setUploader (String uploader)
+    {
+        this.uploader = uploader;
+    }
+
+    public String getImage ()
+    {
+        return image;
+    }
+
+    public void setImage (String image)
+    {
+        this.image = image;
+    }
+
+    public String getDownvotes ()
+    {
+        return downvotes;
+    }
+
+    public void setDownvotes (String downvotes)
+    {
+        this.downvotes = downvotes;
+    }
+
+    public String getUpdated_at ()
+    {
+        return updated_at;
+    }
+
+    public void setUpdated_at (String updated_at)
+    {
+        this.updated_at = updated_at;
+    }
+
+    public String getIs_rendered ()
+    {
+        return is_rendered;
+    }
+
+    public void setIs_rendered (String is_rendered)
+    {
+        this.is_rendered = is_rendered;
+    }
+
+    public String getOrig_sha512_hash ()
+    {
+        return orig_sha512_hash;
+    }
+
+    public void setOrig_sha512_hash (String orig_sha512_hash)
+    {
+        this.orig_sha512_hash = orig_sha512_hash;
+    }
+
+    public String getFaves ()
+    {
+        return faves;
+    }
+
+    public void setFaves (String faves)
+    {
+        this.faves = faves;
+    }
+
+    public String getUploader_id ()
+    {
+        return uploader_id;
+    }
+
+    public void setUploader_id (String uploader_id)
+    {
+        this.uploader_id = uploader_id;
+    }
+}
---
+++ DerpiReq.java
@@ -0,0 +1,18 @@
+package main.java.net.dv8tion;
+
+import com.google.gson.Gson;
+
+public class DerpiReq 
+{
+//	Response answer = new Response(); 
+	private final String mainURL = "https://derpibooru.org/search.json?q=";
+	Gson gson = new Gson();
+	
+	public Response getDerpi(String query) 
+	{
+		query = query.replace(" ", "+");
+		DerpiAPI queryResults = gson.fromJson(HTTPUtils.SendPOSTRequest(mainURL + query).getContent(), DerpiAPI.class);
+		
+		return new Response(queryResults.getSource_url());
+	}
+}
